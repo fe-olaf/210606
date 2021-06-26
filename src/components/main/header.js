d73f7ce40ef3aeb8d5d2c07f693d8c2189f1c613
fe-olaf <frontend.olaf@gmail.com>
2021-06-26 19:26:35 +0900

    feature: 메인 페이지 헤더 추가

---
+++ src/components/main/header.js
@@ -0,0 +1,43 @@
+import './header.scss'
+
+class Header {
+  constructor(props) {
+    this.props = props
+  }
+
+  bindEvent() {
+    const { router, onSignout } = this.props
+
+    // 이벤트 위임 설명
+    document.querySelector('.wrap_user').addEventListener('click', (e) => {
+      if (e.target.className === 'btn_signout') {
+        onSignout()
+      }
+      if (e.target.className === 'btn_signin') {
+        router.push('/signin')
+      }
+    })
+  }
+
+  render() {
+    const { user } = this.props
+
+    return `
+          <div class="wrap_header">
+              <div class="wrap_user">
+                  ${
+                    user
+                      ? `
+                      <button class="btn_signout">로그아웃</button>
+                      <img class="thumbnail" src="${user.thumbnail}">
+                  `
+                      : `
+                      <button class="btn_signin">로그인</button>
+                      `
+                  }
+              </div>
+          </div>`
+  }
+}
+
+export default Header
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,7 +1,49 @@
-class MainPage {
-  constructor() {}
+import Header from '../components/main/header'
 
-  render() {}
+class MainPage {
+  constructor(props) {
+    this.app = document.getElementById('app')
+    this.children = []
+    this.user = JSON.parse(window.localStorage.getItem('user') || 'null')
+    this.props = props
+
+    this.componentUpdate()
+  }
+
+  updateChildrenProps() {
+    this.children = [
+      new Header({
+        parent: this.app,
+        router: this.props.router,
+        onSignout: this.handleSingout.bind(this),
+        user: this.user,
+      }),
+    ]
+  }
+
+  // 컴포넌트에 변화가 일어나면 동작하는 함수
+  componentUpdate() {
+    this.updateChildrenProps()
+    this.render()
+    this.componentbindEvent()
+  }
+
+  handleSingout() {
+    this.user = null
+    window.localStorage.removeItem('user')
+
+    this.componentUpdate()
+  }
+
+  componentbindEvent() {
+    this.children.forEach((children) => children.bindEvent())
+  }
+
+  render() {
+    this.app.innerHTML = this.children.reduce((html, children) => {
+      return `${html}${children.render()}`
+    }, ``)
+  }
 }
 
 export default MainPage
